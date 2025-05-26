2e4568e06ca30eb6ae6182a1412736624b400c1c
Terrocraft <Terrocraft3223@gmail.com>
2025-05-26 16:44:48 +0200

    ups, remove bluemap.

--- CrayonCore/src/main/java/net/crayonsmp/crayonCore/CrayonCore.java
+++ CrayonCore/src/main/java/net/crayonsmp/crayonCore/CrayonCore.java
@@ -1,6 +1,5 @@
 package net.crayonsmp.crayonCore;
 
-import de.bluecolored.bluemap.api.BlueMapAPI;
 import net.crayonsmp.CrayonAPI;
 import net.crayonsmp.interfaces.CrayonModule;
 import net.crayonsmp.utils.config.ConfigUtil;
@@ -59,11 +58,6 @@ public class CrayonCore extends JavaPlugin implements CrayonAPI {
         loadedModules.clear();
         getLogger().info("Finished disabling modules.");
     }
-    private void onBlueMapApiLoaded(BlueMapAPI api){
-        for (CrayonModule module : loadedModules){
-            module.OnBlueMapEnabled(api);
-        }
-    }
     @Override
     public void onLoad() {
         config = ConfigUtil.getConfig("config", this);
@@ -118,7 +112,6 @@ public class CrayonCore extends JavaPlugin implements CrayonAPI {
                 getLogger().log(Level.SEVERE, "!!! CRITICAL FAILURE loading module: " + moduleClassName + " !!!", e);
             }
         }
-        BlueMapAPI.onEnable(this::onBlueMapApiLoaded);
         getLogger().info("Finished module loading sequence. Total modules loaded: " + loadedModules.size());
     }
 
