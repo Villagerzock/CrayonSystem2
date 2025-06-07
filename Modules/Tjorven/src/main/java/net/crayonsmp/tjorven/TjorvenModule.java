81d0e732c533ba7e8c8d4bb8ffb22181b8d98435
Tjorven-Liebe <tjorvenliebe@gmail.com>
2025-06-07 19:51:34 +0200

    feat: Add tjorven module

---
+++ Modules/Tjorven/src/main/java/net/crayonsmp/tjorven/TjorvenModule.java
@@ -0,0 +1,29 @@
+package net.crayonsmp.tjorven;
+
+import net.crayonsmp.CrayonAPI;
+import net.crayonsmp.interfaces.CrayonModule;
+import net.crayonsmp.tjorven.abilities.BlockTeleport;
+import net.crayonsmp.tjorven.abilities.ChainsawAbility;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+
+public class TjorvenModule implements CrayonModule {
+
+    @Override
+    public String getName() {
+        return "Tjorven";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public <API extends Plugin & CrayonAPI> void onEnable(API plugin) {
+        PluginManager pluginManager = plugin.getServer().getPluginManager();
+
+        pluginManager.registerEvents(new BlockTeleport(plugin), plugin);
+        pluginManager.registerEvents(new ChainsawAbility(plugin), plugin);
+    }
+}
